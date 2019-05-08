973d141bf5f3f83f34b00691dabc1d18cb97fca8
HidetoNakasone <highenjoyproject@outlook.jp>
2019-05-08 15:11:49 +0900

    Go Heroku

---
+++ app.rb
@@ -0,0 +1,16 @@
+
+Bundler.require
+
+uri = URI.parse(ENV['DATABASE_URL'])
+connect = PG.connect(
+  host: uri.hostname,
+  dbname: uri.path[1..-1],
+  user: uri.user,
+  port: uri.port,
+  password: uri.password
+)
+
+get '/' do
+  @res = connect.exec('select * from samples;')
+  erb :index
+end
